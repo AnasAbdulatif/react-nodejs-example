2b3e81c9a231260d68341ebe0c6645a2404ee4cc
AnasAbdulatif <anas99anas@gmail.com>
2023-06-16 10:17:45 +0300

    asas

---
+++ script.groovy
@@ -0,0 +1,21 @@
+def buildApp() {
+    echo "building the application... ANAS"
+    sh 'mvn package'
+} 
+
+def buildImage() {
+    echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'dockerhub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh ' docker build -t anas99anas/java-maven-app:jma-2.0 .'
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh ' docker push anas99anas/java-maven-app:jma-2.0'
+    }
+} 
+
+def deployApp() {
+    echo 'deploying the application...'
+    sh ' docker run --name javamaven anas99anas/java-maven-app:jma-2.0'
+    sh ' docker logs javamaven'
+} 
+
+return this
